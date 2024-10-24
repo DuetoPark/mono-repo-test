0645f5446c4a54ac8879baaaeb295cf621b139a1
DuetoPark <inhye.park.dev@gmail.com>
2024-10-24 23:28:32 +0900

    🔧 [config] git cache 초기화

--- packages/design-system/src/App.tsx
+++ packages/design-system/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <div className="example"></div>
+      <div className="example">aaa</div>
     </div>
   );
 }
